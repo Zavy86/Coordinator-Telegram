6afcac4b3157c2059e469ffea2b58ffd9cae0115
Manuel Zavatta <manuel.zavatta@gmail.com>
2020-01-23 11:09:43 +0100

    Module informations

---
+++ module.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Telegram
+ *
+ * @package Coordinator\Modules\Telegram
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.coordinator.it
+ */
+ // definitions
+ $module_name="Telegram";
+ $module_repository_url="https://github.com/Zavy86/Coordinator-Telegram/";
+ $module_repository_version_url="https://raw.githubusercontent.com/Zavy86/Coordinator-Telegram/master/VERSION.txt";
+ $module_required_modules=array();
+?>
